9b25215c3c4d066383c5f050cc04e131ca024a5f
EugeniuPeruzzi <jaka1997@live.it>
2023-07-21 16:17:22 +0200

    for each in main, problem no img

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -23,10 +23,13 @@
     <div class="jumbotron-black">
                 <div class="container">
                     <div class="row">
-                        <div class="col">
+                        <div class="col d-flex flex-wrap">
                             @foreach($products as $product)    
-                                <div class="card-container d-flex flex-wrap">
-            
+                                <div class="card" style="width: 18rem;">
+                                    <img src=" {{ $product['thumb'] }} " class="card-img-top" alt="">
+                                    <div class="card-body">
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    </div>
                                 </div>
                             @endforeach
                         </div>
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    $prducts = config('db.comics');
+    $products = config('db.comics');
+
     return view('home', compact('products'));
 }) -> name('home');
 
