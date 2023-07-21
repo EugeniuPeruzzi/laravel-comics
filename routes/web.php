ac54c2e44ba52cfc5c6079e2f93ecffd42d28815
EugeniuPeruzzi <jaka1997@live.it>
2023-07-21 15:49:50 +0200

    getting data from db

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('home');
+    $prducts = config('db.comics');
+    return view('home', compact('products'));
 }) -> name('home');
+
